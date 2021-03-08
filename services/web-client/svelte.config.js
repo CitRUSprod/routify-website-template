76338f12b34ec50169222d6ff9aeb7607d02855a
CitRUS <citrus4009@gmail.com>
2021-03-08 22:26:00 +0300

    fix: Fix svelte files formatting

--- services/web-client/svelte.config.js
+++ services/web-client/svelte.config.js
@@ -1,10 +1,11 @@
+const path = require("path")
 const sveltePreprocess = require("svelte-preprocess")
 const { preprocess: windicss } = require("svelte-windicss-preprocess")
 
 module.exports = {
     preprocess: [
         windicss({
-            config: "tailwind.config.js",
+            config: path.join(__dirname, "tailwind.config.js"),
             compile: true,
             globalPreflight: true,
             globalUtility: true
