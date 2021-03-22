c041173ab1f6b52b2959ddae5283d60d27fad587
CitRUS <citrus4009@gmail.com>
2021-03-22 22:41:12 +0300

    refactor(web-client): Refactor message system

--- services/web-client/src/pages/_components/TheMessages.svelte
+++ services/web-client/src/pages/_components/TheMessages.svelte
@@ -4,12 +4,14 @@
     import { fly, fade } from "svelte/transition"
     import { mdiClose } from "@mdi/js"
     import { messages } from "@/stores"
+
+    const { list } = messages
 </script>
 
-<div class="space-y-2 text-white right-4 bottom-4 w-60 absolute">
-    {#each $messages as message (message.id)}
+<div class="flex flex-col-reverse text-white right-4 bottom-3 w-60 absolute">
+    {#each $list as message (message.id)}
         <div
-            class="rounded-lg flex shadow p-3 items-center select-none"
+            class="rounded-lg flex shadow my-1 p-3 items-center select-none"
             class:success="{message.type === 'success'}"
             class:error="{message.type === 'error'}"
             class:warning="{message.type === 'warning'}"
@@ -17,7 +19,7 @@
             in:fly="{{ y: -200, duration: 500 }}"
             out:fade
         >
-            <div class="flex-grow pr-3 break-words">{message.text}</div>
+            <div class="flex-grow pr-3 break-text">{message.text}</div>
             <div>
                 <Button
                     color="{message.type}"
@@ -32,6 +34,9 @@
 </div>
 
 <style lang="postcss">
+    .break-text {
+        word-break: break-word;
+    }
     .success {
         @apply bg-success;
     }
--- services/web-client/src/stores/messages.ts
+++ services/web-client/src/stores/messages.ts
@@ -1,42 +1,48 @@
-import { writable } from "svelte/store"
+import { writable, derived } from "svelte/store"
 
-type MessageType = "success" | "error" | "warning" | "info"
+type Type = "success" | "error" | "warning" | "info"
 
 interface Message {
     id: number
-    type: MessageType
+    type: Type
     text: string
 }
 
+type Messages = Array<Readonly<Message>>
+
 function createMessages() {
-    const { subscribe, update } = writable<Array<Message>>([])
+    const writableList = writable<Messages>([])
+    const list = derived(writableList, l => l as Readonly<Messages>)
 
-    return {
-        subscribe,
-        add(type: MessageType, text: string, timeout = 5000) {
-            update(messages => {
-                const id = Date.now()
-                const message: Message = {
-                    id,
-                    type,
-                    text: text.trim()
-                }
-                messages = [message, ...messages]
+    const { update } = writableList
 
-                setTimeout(() => {
-                    this.remove(id)
-                }, timeout)
-
-                return messages
-            })
-        },
-        remove(id: number) {
-            update(messages => {
-                messages = messages.filter(msg => msg.id !== id)
-                return messages
-            })
-        }
+    function remove(id: number) {
+        update(messages => {
+            const index = messages.findIndex(msg => msg.id === id)
+            messages.splice(index, 1)
+            return messages
+        })
     }
+
+    function add(type: Type, text: string, timeout = 5000) {
+        update(messages => {
+            const id = Date.now()
+            const message: Message = {
+                id,
+                type,
+                text: text.trim()
+            }
+            messages.push(message)
+
+            setTimeout(() => {
+                remove(id)
+            }, timeout)
+
+            return messages
+        })
+    }
+
+    return { list, add, remove }
 }
 
 export const messages = createMessages()
